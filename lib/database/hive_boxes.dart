726aa7c779f4171395d951c498e5a9075b86c762
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-08 00:21:51 +0200

    Register bag inventory Hive box

--- lib/database/hive_boxes.dart
+++ lib/database/hive_boxes.dart
@@ -3,6 +3,7 @@ class HiveBoxes {
   static const pokedexEntries = 'pokedex_entries';
   static const teams = 'teams';
   static const pcPokemon = 'pc_pokemon';
+  static const bagItems = 'bag_items';
   static const settings = 'settings';
   static const appState = 'app_state';
 }

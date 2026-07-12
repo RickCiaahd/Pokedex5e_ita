3c8232e58cb0a958bf8cfed67ff14b07c53d7b9a
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-12 20:08:44 +0200

    Register encounter collection storage

--- lib/database/hive_boxes.dart
+++ lib/database/hive_boxes.dart
@@ -5,6 +5,7 @@ class HiveBoxes {
   static const pcPokemon = 'pc_pokemon';
   static const bagItems = 'bag_items';
   static const battleSessions = 'battle_sessions';
+  static const encounterCollections = 'encounter_collections';
   static const settings = 'settings';
   static const appState = 'app_state';
 }

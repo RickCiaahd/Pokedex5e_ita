69fd6007f58f24f01a16f1b1e90e55d89ca716b2
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-14 12:00:22 +0200

    Aggiunge test della scheda caratteristiche

---
+++ test/pokemon_battle_attributes_card_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pokedex_5e_ita/widgets/battle/pokemon_battle_attributes_card.dart';
+
+void main() {
+  testWidgets('mostra tutte le caratteristiche e i modificatori', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: SingleChildScrollView(
+            child: PokemonBattleAttributesCard(
+              attributes: {
+                'STR': 8,
+                'DEX': 12,
+                'CON': 14,
+                'INT': 10,
+                'WIS': 16,
+                'CHA': 6,
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('CARATTERISTICHE'), findsOneWidget);
+    expect(find.text('Forza'), findsOneWidget);
+    expect(find.text('Destrezza'), findsOneWidget);
+    expect(find.text('Costituzione'), findsOneWidget);
+    expect(find.text('Intelligenza'), findsOneWidget);
+    expect(find.text('Saggezza'), findsOneWidget);
+    expect(find.text('Carisma'), findsOneWidget);
+    expect(find.text('-1'), findsOneWidget);
+    expect(find.text('+1'), findsOneWidget);
+    expect(find.text('+2'), findsOneWidget);
+    expect(find.text('+0'), findsOneWidget);
+    expect(find.text('+3'), findsOneWidget);
+    expect(find.text('-2'), findsOneWidget);
+  });
+}

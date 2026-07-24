04103339b324f7d812fc3983d641af2ccdd1e7a1
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-24 02:58:12 +0200

    Registra il completamento del tour della Home

--- lib/database/hive_keys.dart
+++ lib/database/hive_keys.dart
@@ -1,4 +1,5 @@
 class HiveKeys {
   static const activeProfileId = 'active_profile_id';
   static const onboardingCompleted = 'onboarding_completed';
+  static const homeTourCompleted = 'home_tour_completed';
 }

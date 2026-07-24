03d7af01ee3b113b5db6ba29785d2d32a6ac2f3b
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-24 10:53:39 +0200

    Aggiunge il test per larghezze minime nella card caratteristiche

--- test/pokemon_battle_attributes_card_test.dart
+++ test/pokemon_battle_attributes_card_test.dart
@@ -4,21 +4,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pokedex_5e_ita/widgets/battle/pokemon_battle_attributes_card.dart';
 
 void main() {
+  const attributes = {
+    'STR': 8,
+    'DEX': 12,
+    'CON': 14,
+    'INT': 10,
+    'WIS': 16,
+    'CHA': 6,
+  };
+
   testWidgets('mostra tutte le caratteristiche e i modificatori', (tester) async {
     await tester.pumpWidget(
       const MaterialApp(
         home: Scaffold(
           body: SingleChildScrollView(
-            child: PokemonBattleAttributesCard(
-              attributes: {
-                'STR': 8,
-                'DEX': 12,
-                'CON': 14,
-                'INT': 10,
-                'WIS': 16,
-                'CHA': 6,
-              },
-            ),
+            child: PokemonBattleAttributesCard(attributes: attributes),
           ),
         ),
       ),
@@ -43,5 +43,25 @@ void main() {
     expect(find.text('+0'), findsOneWidget);
     expect(find.text('+3'), findsOneWidget);
     expect(find.text('-2'), findsOneWidget);
+  });
+
+  testWidgets('non genera vincoli negativi con larghezza quasi nulla',
+      (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Align(
+            alignment: Alignment.topLeft,
+            child: SizedBox(
+              width: 24,
+              child: PokemonBattleAttributesCard(attributes: attributes),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump();
+    expect(tester.takeException(), isNull);
   });
 }

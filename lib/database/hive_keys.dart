f6cdfe7e12b00077ff7273ce2b6cb7a8df614e99
RickCiaahd <59015717+RickCiaahd@users.noreply.github.com>
2026-07-24 09:42:23 +0200

    Aggiunge la chiave dei tour guidati versionati

--- lib/database/hive_keys.dart
+++ lib/database/hive_keys.dart
@@ -2,4 +2,5 @@ class HiveKeys {
   static const activeProfileId = 'active_profile_id';
   static const onboardingCompleted = 'onboarding_completed';
   static const homeTourCompleted = 'home_tour_completed';
+  static const guidedToursCompleted = 'guided_tours_completed';
 }
